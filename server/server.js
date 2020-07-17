022409f25d08bcba4c18f9e6e56a690f5a693c27
JannisKohle <Jannis.Kohle@icloud.com>
2020-07-17 19:43:09 +0200

    loaded rooms Router in server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const accountsRoutes = require("./routes/accounts");
+const roomsRoutes = require("./routes/rooms");
 const app = express();
 
 app.use(express.json());
 
 app.use("/accounts", accountsRoutes);
+app.use("/rooms", roomsRoutes);
 
 module.exports = app;
